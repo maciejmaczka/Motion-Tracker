84b6956e83d5605d45ee3eb9df840cfe30da9df0
maciejmaczka <maciej.maczka@gmail.com>
2022-06-23 12:36:06 +0200

    first upload

---
+++ src/compvisionfilter/trackpointer.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package compvisionfilter;
+
+/**
+ *
+ * @author mac
+ */
+public class trackpointer 
+{
+    public boolean  inuse = true;
+    public int x = 0;
+    public int y = 0;
+    
+    public int color = 0;
+    
+}
